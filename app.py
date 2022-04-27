f28a51525221734c25d42647fd58709ae17580f9
ErnestasKodis <43343716+ErnestasKodis@users.noreply.github.com>
2022-04-27 10:27:12 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -15,25 +15,29 @@ movies = [
     "title" : "Uncharted",
     "director" : "Ruben Fleischer",
     "rating" : 6.6,
-    "release_year" : 2022
+    "release_year" : 2022,
+    "genre" : "Adventure"
     },
     {"id" : 2,
     "title" : "Fight Club",
     "director" : "David Fincher",
     "rating" : 8.8,
-    "release_year" :1999
+    "release_year" :1999,
+    "genre" : "Drama"
     },
     {"id" : 3,
     "title" : "The Godfather",
     "director" : "Francis Ford Coppola",
     "rating" : 9.2,
-    "release_year" : 1972
+    "release_year" : 1972,
+    "genre" : "Crime"
     },
     {"id" : 4,
     "title" : "The Dark Knight",
     "director" : "Christopher Nolan",
     "rating" : 9.0,
-    "release_year" : 2008
+    "release_year" : 2008,
+    "genre" : "Action"
     }]
 
 @application.route('/api/movies', methods = ["GET"])  # Returns full movie list
@@ -60,9 +64,13 @@ def add_movie():
             "rating" : new_movie_data["rating"],
             "release_year" : new_movie_data["release_year"]
         }
+        if  "genre" in new_movie_data:
+            new_movie["genre"] = new_movie_data["genre"]
+        else:
+            new_movie["genre"] = ""
         movies.append(new_movie)
         current_id += 1
-        return Response((json.dumps({"Success":"Movies was added to the list;" + str(jsonify(new_movie))})), status = 201, headers={"location": "/api/movies/"+str(new_movie["id"]-1 )}, mimetype="application/json")
+        return Response(json.dumps(new_movie), status = 201, headers={"location": "/api/movies/"+str(new_movie["id"] )}, mimetype="application/json")
     else:
         error = "Missing "
         if "title" not in new_movie_data:
@@ -75,11 +83,11 @@ def add_movie():
             error += "release year; "
         return Response(json.dumps({"Failure" : error}, status=400, mimetype="application/json"))
 
-@application.route("/api/movies/<int:movieId>", methods = ["PATCH"])
+@application.route("/api/movies/<int:movieId>", methods = ["PUT"])
 def put_movie(movieId):
     movieId -= 1
     new_movie_data = request.get_json()
-    if "title" not in new_movie_data and "director" not in new_movie_data and "rating" not in new_movie_data and "release_year" not in new_movie_data:
+    if "title" not in new_movie_data or "director" not in new_movie_data or "rating" not in new_movie_data or "release_year" not in new_movie_data:
         error = "Missing "
         if "title" not in new_movie_data:
             error += "title; "
@@ -90,11 +98,11 @@ def put_movie(movieId):
         if "release_year" not in new_movie_data:
             error += "release year; "
         error += ". Please specify Body"
-        return Response(json.dumps({"Failure" : error}),status=404, mimetype="application/json")
+        return Response(json.dumps({"Failure" : error}),status=400, mimetype="application/json")
     message = ""
     if "title" in new_movie_data:
         movies[movieId]["title"] = new_movie_data["title"]
-        message += "Model; "
+        message += "title; "
     if "director" in new_movie_data:
         movies[movieId]["director"] = new_movie_data["director"]
         message += "Director; "
@@ -104,6 +112,35 @@ def put_movie(movieId):
     if "release_year" in new_movie_data:
         movies[movieId]["release_year"] = new_movie_data["release_year"]
         message += "Release year; "
+    if "genre" in new_movie_data:
+        movies[movieId]["genre"] = new_movie_data["genre"]
+        message += "genre; "
+    message += "have been changed"
+    return Response(json.dumps(movies[movieId]), status=200, mimetype="application/json")
+
+@application.route("/api/movies/<int:movieId>", methods = ["PATCH"])
+def patch_movie(movieId):
+    movieId -= 1
+    new_movie_data = request.get_json()
+    if "title" not in new_movie_data and "director" not in new_movie_data and "rating" not in new_movie_data and "release_year" not in new_movie_data:
+        error = "Wrong request body"
+        return Response(json.dumps({"Failure" : error}),status=400, mimetype="application/json")
+    message = ""
+    if "title" in new_movie_data:
+        movies[movieId]["title"] = new_movie_data["title"]
+        message += "title; "
+    if "director" in new_movie_data:
+        movies[movieId]["director"] = new_movie_data["director"]
+        message += "Director; "
+    if "rating" in new_movie_data:
+        movies[movieId]["rating"] = new_movie_data["rating"]
+        message  += "Rating; "
+    if "release_year" in new_movie_data:
+        movies[movieId]["release_year"] = new_movie_data["release_year"]
+        message += "Release year; "
+    if "genre" in new_movie_data:
+        movies[movieId]["genre"] = new_movie_data["genre"]
+        message += "genre; "
     message += "have been changed"
     return Response(json.dumps(movies[movieId]), status=200, mimetype="application/json")
 
@@ -114,7 +151,7 @@ def delete_movie(movieId):
             movies.remove(movie)
             return Response(response=(json.dumps({"Success" : "Deleted"})), status=204, mimetype="application/json")
     error = "Movie with ID: " + str(movieId) + " wasn't found"
-    return Response(json.dumps({"Failure": error}), status=400, mimetype="application/json")
+    return Response(json.dumps({"Failure": error}), status=404, mimetype="application/json")
 
 
 if __name__ == '__main__':
